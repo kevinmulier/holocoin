bdedcc4ee215a5b95ff5e2ba45074f888dcbeea4
kevinmulier <kevin.mulier@outlook.fr>
2024-03-09 23:43:41 +0100

    complete basic navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,60 +1,37 @@
 import { AlignJustify } from 'lucide-react';
 
 const Navbar = () => {
+  const menuItems = ['Home', 'About', 'Blog', 'Roadmap', 'Team', 'Rarity', 'Contact'];
+
   return (
-    <nav className="navbar">
-      <div className="navbar-start">
-        <a href="#" className="btn btn-ghost text-xl flex items-center">
+    <nav className="navbar pt-8 py-5 w-full max-w-7xl justify-between mx-auto px-6">
+      <div className="flex gap-16 items-baseline">
+        <a href="#" className="text-2xl flex gap-2 tracking-widest font-bold">
           <img src="/logo/logo.svg" alt="Holocoin logo" className="h-6" />
           HOLOCOIN
         </a>
+        <div className="hidden lg:flex">
+          <ul className="flex gap-8">
+            {menuItems.map((item) => (
+              <li key={item} className="font-bold">
+                <a href="#">{item}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end lg:hidden">
+      <button className="btn rounded-full btn-accent max-lg:hidden min-h-0 h-fit py-3">Buy Holocoin</button>
+      <div className="lg:hidden">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <AlignJustify />
           </div>
           <ul tabIndex={0} className="menu menu-sm dropdown-content z-[1]">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {menuItems.map((item) => (
+              <li key={item}>
+                <a href="#">{item}</a>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
