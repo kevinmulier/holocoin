818498e0d22108f9ba7b65e88d3e1a14dee972ee
kevinmulier <kevin.mulier@outlook.fr>
2024-03-09 21:56:10 +0100

    add logo and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 import daisyui from 'daisyui';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [daisyui],
 };
