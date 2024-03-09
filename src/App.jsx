1b5e91f79865f1149df3a17dd46200fa22fe9593
kevinmulier <kevin.mulier@outlook.fr>
2024-03-10 00:12:20 +0100

    complete navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Navbar from './components/navbar/Navbar';
 function App() {
   return (
     <>
-      <Navbar />
+      <div className="border-b border-black/30 shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]">
+        <Navbar />
+      </div>
     </>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,22 +5,24 @@ const Navbar = () => {
 
   return (
     <nav className="navbar pt-8 py-5 w-full max-w-7xl justify-between mx-auto px-6">
-      <div className="flex gap-16 items-baseline">
-        <a href="#" className="text-2xl flex gap-2 tracking-widest font-bold">
-          <img src="/logo/logo.svg" alt="Holocoin logo" className="h-6" />
+      <div className="flex gap-16 items-center">
+        <a href="#" className="text-lg flex items-baseline gap-2 tracking-widest font-bold">
+          <img src="/logo/logo.svg" alt="Holocoin logo" className="h-4" />
           HOLOCOIN
         </a>
         <div className="hidden lg:flex">
           <ul className="flex gap-8">
             {menuItems.map((item) => (
-              <li key={item} className="font-bold">
+              <li key={item} className="hover:text-primary hover:font-extrabold text-primary/60 font-semibold">
                 <a href="#">{item}</a>
               </li>
             ))}
           </ul>
         </div>
       </div>
-      <button className="btn rounded-full btn-accent max-lg:hidden min-h-0 h-fit py-3">Buy Holocoin</button>
+      <button className="btn rounded-full btn-secondary text-primary max-lg:hidden min-h-0 h-fit py-3 font-semibold">
+        Buy Holocoin
+      </button>
       <div className="lg:hidden">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
