d275f5b5068ba071f4decbb1a384da5a654d574a
kevinmulier <kevin.mulier@outlook.fr>
2024-03-10 13:13:32 +0100

    improve mobile display (no shadow, improved menu...)

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/navbar/Navbar';
 function App() {
   return (
     <>
-      <div className="border-b border-black/30 shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]">
+      <div className="lg:border-b border-black/30 lg:shadow-[0.0px_4.0px_4.0px_rgba(0,0,0,0.38)]">
         <Navbar />
       </div>
     </>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
   const menuItems = ['Home', 'About', 'Blog', 'Roadmap', 'Team', 'Rarity', 'Contact'];
 
   return (
-    <nav className="navbar pt-8 pb-4 w-full max-w-7xl justify-between mx-auto px-6">
+    <nav className="justify-between w-full px-6 pt-8 pb-4 mx-auto navbar max-w-7xl">
       <div className="flex gap-[72px] items-center">
-        <a href="#" className="text-lg flex items-baseline gap-2 tracking-widest font-bold">
+        <a href="#" className="flex items-baseline gap-2 text-lg font-bold tracking-widest">
           <img src="/logo/logo.svg" alt="Holocoin logo" className="h-4" />
           HOLOCOIN
         </a>
@@ -19,10 +19,10 @@ const Navbar = () => {
                 key={item}
                 onMouseEnter={() => setActiveIndex(i)}
                 onMouseLeave={() => setActiveIndex(0)}
-                className="hover:text-primary hover:font-extrabold text-primary/60 font-semibold flex flex-col items-center gap-1 relative">
+                className="relative flex flex-col items-center gap-1 font-semibold hover:text-primary hover:font-extrabold text-primary/60">
                 {i === activeIndex && (
                   <>
-                    <a href="#" className="text-primary font-extrabold px-6">
+                    <a href="#" className="px-6 font-extrabold text-primary">
                       {item}
                     </a>
                     <span className="absolute top-7 h-[3px] rounded-tl-full rounded-tr-full w-5 bg-secondary"></span>
@@ -38,7 +38,7 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
-      <button className="btn rounded-full btn-secondary text-primary max-lg:hidden min-h-0 h-fit py-3 font-semibold">
+      <button className="min-h-0 py-3 font-semibold rounded-full btn btn-secondary text-primary max-lg:hidden h-fit">
         Buy Holocoin
       </button>
       <div className="lg:hidden">
@@ -46,10 +46,12 @@ const Navbar = () => {
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <AlignJustify />
           </div>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content z-[1]">
+          <ul tabIndex={0} className="menu menu-sm dropdown-content right-0 z-[10] bg-neutral rounded-box">
             {menuItems.map((item) => (
               <li key={item}>
-                <a href="#">{item}</a>
+                <a href="#" className="px-5 text-lg">
+                  {item}
+                </a>
               </li>
             ))}
           </ul>
