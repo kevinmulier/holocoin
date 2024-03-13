0d31da4a824972f10ec5ef9a917c8243a3f1b045
kevinmulier <kevin.mulier@outlook.fr>
2024-03-13 03:28:33 +0100

    fix width of title underline

--- src/components/main/team/TeamTitle.jsx
+++ src/components/main/team/TeamTitle.jsx
@@ -2,7 +2,7 @@ const TeamTitle = () => {
   return (
     <div className="relative flex flex-col mb-8 md:ml-6">
       <h2 className="font-semibold text-[32px] text-primary">Team</h2>
-      <span className="absolute top-11 left-5 h-[3px] rounded-tl-full rounded-tr-full w-12 bg-secondary"></span>
+      <span className="absolute top-11 left-5 h-[3px] rounded-tl-full rounded-tr-full w-11 bg-secondary"></span>
     </div>
   );
 };
