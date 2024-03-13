c2dab66283bd100a896a3133fb52b2a7269b0966
kevinmulier <kevin.mulier@outlook.fr>
2024-03-13 03:27:13 +0100

    add second gradient on larger screen + adapt aspect ratio on smaller screens

--- src/components/main/VideoPlayer.jsx
+++ src/components/main/VideoPlayer.jsx
@@ -2,12 +2,18 @@ import { Play } from 'lucide-react';
 
 const VideoPlayerMain = () => {
   return (
-    <div className="relative flex justify-center items-center w-full mx-auto max-w-[2560px]">
-      <div className="absolute inset-0 h-full bg-gradient-to-b from-base-100/90 from-[5%] via-base-100/0 to-[95%] to-base-100/90"></div>
-      <button className="absolute [padding:_clamp(1.25rem,2.083vw+0.833rem,2.5rem)] bg-transparent border-4 rounded-full btn border-primary text-primary h-fit w-fit">
+    <div className="relative flex justify-center items-center w-full mx-auto max-w-[1536px]">
+      <div className="absolute inset-0 h-full bg-gradient-to-b from-base-100 from-[0.1%] via-transparent to-[99.9%] to-base-100 max-md:hidden"></div>
+      <div className="absolute inset-0 h-full bg-gradient-to-b from-neutral from-[0.1%] via-transparent to-[99.9%] to-base-100 md:hidden"></div>
+      <div className="absolute inset-0 bg-gradient-to-r from-base-100 from-[0.1%] via-transparent to-[99.9%] to-base-100 max-2xl:hidden"></div>
+      <button className="absolute [padding:_clamp(1.25rem,2.083vw+0.833rem,2.5rem)] bg-transparent border-4 rounded-full btn border-primary text-primary h-fit w-fit hover:bg-secondary hover:border-secondary">
         <Play className="fill-current [height:_clamp(1.875rem,6.25vw+0.625rem,5.625rem)] [width:_clamp(1.875rem,6.25vw+0.625rem,5.625rem)]" />
       </button>
-      <img src="/images/lol_image.webp" alt="League of Legends NFTs" className="w-full" />
+      <img
+        src="/images/lol_image.webp"
+        alt="League of Legends NFTs"
+        className="object-cover max-lg:aspect-video lg:w-full"
+      />
     </div>
   );
 };
