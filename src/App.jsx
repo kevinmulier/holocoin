3f224dea52b59446443b4ef5836ee15e683a163a
kevinmulier <kevin.mulier@outlook.fr>
2024-03-14 12:57:16 +0100

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from './components/footer/Footer';
 import Hero from './components/hero/Hero';
 import Main from './components/main/Main';
 import Navbar from './components/navbar/Navbar';
@@ -5,11 +6,10 @@ import Navbar from './components/navbar/Navbar';
 function App() {
   return (
     <>
-      <header className="lg:border-b border-black/30 lg:shadow-[0.0px_4.0px_4.0px_rgba(0,0,0,0.38)] max-lg:sticky max-lg:top-0 max-lg:z-[100] backdrop-blur-lg">
-        <Navbar />
-      </header>
+      <Navbar />
       <Hero />
       <Main />
+      <Footer />
     </>
   );
 }
