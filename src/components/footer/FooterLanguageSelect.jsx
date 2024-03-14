f6344c8b641440f4786c537f17cd8074efdaece3
kevinmulier <kevin.mulier@outlook.fr>
2024-03-14 15:47:37 +0100

    refactor language select into its own component

---
+++ src/components/footer/FooterLanguageSelect.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { ChevronDown } from 'lucide-react';
+import FooterLanguageSelectOption from './FooterLanguageSelectOption';
+
+const FooterLanguageSelect = () => {
+  const [selectedLanguage, setSelectedLanguage] = useState({ flagSrc: '/logo/en_rounded.png', language: 'English' });
+
+  const languages = [
+    { flagSrc: '/logo/en_rounded.png', language: 'English' },
+    { flagSrc: '/logo/fr_rounded.png', language: 'Français' },
+  ];
+
+  return (
+    <div className="flex justify-center dropdown dropdown-hover dropdown-top w-36">
+      <div tabIndex={0} role="button" className="font-normal btn btn-ghost no-animation hover:bg-transparent">
+        <img src={selectedLanguage.flagSrc} alt={`${selectedLanguage.language} flag`} /> {selectedLanguage.language}{' '}
+        <ChevronDown size={16} />
+      </div>
+      <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 bg-primary/5 backdrop-blur-lg rounded-box">
+        {languages.map((language) => (
+          <FooterLanguageSelectOption
+            languageObject={language}
+            setSelectedLanguage={setSelectedLanguage}
+            key={language.language}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FooterLanguageSelect;
