8acc39f81bd8a4776815d0bb47b3119546c0259a
kevinmulier <kevin.mulier@outlook.fr>
2024-03-14 11:44:20 +0100

    fix padding

--- src/components/main/rarity/Rarity.jsx
+++ src/components/main/rarity/Rarity.jsx
@@ -4,8 +4,8 @@ import RarityTitle from './RarityTitle';
 const Rarity = () => {
   return (
     <>
-      <div className="w-full bg-neutral">
-        <section className="flex flex-col w-full py-12 mx-auto  max-w-[1512px] px-7">
+      <div className="w-full py-16 bg-neutral">
+        <section className="flex flex-col w-full mx-auto max-w-[1512px] px-7">
           <RarityTitle />
           <RarityCarouselDesktop />
           <RarityCarouselMobile />
--- src/components/main/team/Team.jsx
+++ src/components/main/team/Team.jsx
@@ -5,21 +5,16 @@ import TeamTitle from './TeamTitle';
 const Team = () => {
   return (
     <>
-      <section className="flex flex-col w-full py-12 mx-auto  max-w-[1512px] px-7">
+      <section className="flex flex-col w-full mx-auto max-w-[1512px] px-7 py-16">
         <TeamTitle />
-        <section className="flex justify-between gap-24 px-12 mt-4 max-lg:hidden">
-          <div className="flex flex-col justify-center w-3/12">
-            <AvatarCard />
-          </div>
-          <div className="flex flex-col justify-center w-3/12">
-            <AvatarCard />
-          </div>
-          <div className="flex flex-col justify-center w-3/12">
-            <AvatarCard />
-          </div>
-          <div className="flex flex-col justify-center w-3/12">
-            <AvatarCard />
-          </div>
+        <section className="flex justify-center gap-24 px-12 mt-4 max-lg:hidden">
+          {Array(4)
+            .fill()
+            .map((_, index) => (
+              <div className="flex flex-col justify-center w-3/12" key={index}>
+                <AvatarCard />
+              </div>
+            ))}
         </section>
         <TeamCarousel />
       </section>
