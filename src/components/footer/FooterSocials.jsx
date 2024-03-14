d565f4fd8684b1fc0268d58903b2795d19178459
kevinmulier <kevin.mulier@outlook.fr>
2024-03-14 23:10:47 +0100

    add width and height to icons

--- src/components/footer/FooterSocials.jsx
+++ src/components/footer/FooterSocials.jsx
@@ -1,33 +1,17 @@
 const FooterSocials = () => {
   return (
     <div className="flex items-center justify-center gap-7 md:gap-4 max-md:mt-10">
-      <a
-        href="#"
-        className="hover:brightness-75 md:[width:clamp(0.75rem,0.375rem+0.7813vw,1rem)]"
-        width="16"
-        height="16">
-        <img src="/logo/twitter_icon.svg" alt="Twitter" />
+      <a href="#" className="hover:brightness-75 md:[width:clamp(0.75rem,0.375rem+0.7813vw,1rem)]">
+        <img src="/logo/twitter_icon.svg" alt="Twitter" width="16" height="16" />
       </a>
-      <a
-        href="#"
-        className="hover:brightness-75 md:[width:clamp(0.75rem,0.375rem+0.7813vw,1rem)]"
-        width="16"
-        height="16">
-        <img src="/logo/fb_icon.svg" alt="Facebook" />
+      <a href="#" className="hover:brightness-75 md:[width:clamp(0.75rem,0.375rem+0.7813vw,1rem)]">
+        <img src="/logo/fb_icon.svg" alt="Facebook" width="16" height="16" />
       </a>
-      <a
-        href="#"
-        className="hover:brightness-75 md:[width:clamp(0.75rem,0.375rem+0.7813vw,1rem)]"
-        width="16"
-        height="16">
-        <img src="/logo/ig_icon.svg" alt="Instagram" />
+      <a href="#" className="hover:brightness-75 md:[width:clamp(0.75rem,0.375rem+0.7813vw,1rem)]">
+        <img src="/logo/ig_icon.svg" alt="Instagram" width="16" height="16" />
       </a>
-      <a
-        href="#"
-        className="hover:brightness-75 md:[width:clamp(0.75rem,0.375rem+0.7813vw,1rem)]"
-        width="16"
-        height="16">
-        <img src="/logo/discord_icon.svg" alt="Discord" />
+      <a href="#" className="hover:brightness-75 md:[width:clamp(0.75rem,0.375rem+0.7813vw,1rem)]">
+        <img src="/logo/discord_icon.svg" alt="Discord" width="16" height="16" />
       </a>
     </div>
   );
