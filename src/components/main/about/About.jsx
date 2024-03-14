9d3c635bbd5697524170441f66c4485843cc49ab
kevinmulier <kevin.mulier@outlook.fr>
2024-03-14 22:38:37 +0100

    upscale avatar card size

--- src/components/main/about/About.jsx
+++ src/components/main/about/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
     <>
       <section className="flex justify-between w-full py-12 mx-auto max-md:flex-col max-md:bg-neutral max-w-[1512px] px-7 md:pl-6 md:pr-20 ">
         <AboutTitle desktopHide={true} />
-        <div className="flex flex-col justify-center pt-4 mx-auto w-fit max-md:mb-12">
+        <div className="flex flex-col justify-center pt-4 mx-auto w-fit max-w-60 max-md:mb-12">
           <AvatarCard />
         </div>
         <div className="flex flex-col w-[47%] leading-tight max-md:w-full">
