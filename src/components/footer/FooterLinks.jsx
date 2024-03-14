f3424f83f6a8616c7b96617e89d40d2362ebcc49
kevinmulier <kevin.mulier@outlook.fr>
2024-03-14 16:40:21 +0100

    add clamp on font size

--- src/components/footer/FooterLinks.jsx
+++ src/components/footer/FooterLinks.jsx
@@ -1,6 +1,6 @@
 const FooterLinks = () => {
   return (
-    <nav className="flex items-center justify-center gap-4 font-semibold md:gap-12 max-md:flex-col text-accent">
+    <nav className="flex justify-center gap-4 font-semibold lg:gap-12 max-md:flex-col text-accent md:[font-size:clamp(0.75rem,0.375rem+0.7813vw,1rem)] text-center items-center">
       <a href="#" className="hover:text-primary">
         Privacy Policy
       </a>
