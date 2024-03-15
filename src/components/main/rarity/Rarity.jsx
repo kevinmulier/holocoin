befffd752ab37714abeced7fe13ba64838465a0d
kevinmulier <kevin.mulier@outlook.fr>
2024-03-15 17:14:53 +0100

    fix issue with navbar not appearing while rarity is not visible

--- src/components/main/rarity/Rarity.jsx
+++ src/components/main/rarity/Rarity.jsx
@@ -5,18 +5,18 @@ import { motion } from 'framer-motion';
 const Rarity = () => {
   return (
     <>
-      <div className="w-full py-16 bg-neutral">
-        <motion.section
-          initial={{ opacity: 0, x: 100 }}
-          whileInView={{ opacity: 1, x: 0 }}
-          viewport={{ once: true, amount: 0.6 }}
-          transition={{ duration: 0.4 }}
-          className="flex flex-col w-full mx-auto max-w-[1512px] px-7">
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: true, amount: 0.6 }}
+        transition={{ duration: 0.4 }}
+        className="w-full py-16 bg-neutral">
+        <section className="flex flex-col w-full mx-auto max-w-[1512px] px-7">
           <RarityTitle />
           <RarityCarouselDesktop />
           <RarityCarouselMobile />
-        </motion.section>
-      </div>
+        </section>
+      </motion.div>
     </>
   );
 };
